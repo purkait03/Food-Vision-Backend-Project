c58c2b1be36532a2a8ad7dfeb3075f563268ccc4
purkait03 <purkaitsougata@gmail.com>
2025-12-21 02:44:16 +0530

    Add refresh access-token functionality and update user routes to include refresh-token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,45 +1,44 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.model.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
 import mongoose from "mongoose"
 import jwt from "jsonwebtoken"
 
 const options = {
-    httpOnly : true,
+    httpOnly: true,
     secure: true
 }
 
 
-const generateAccessAndRefreshToken = async (userId)=>{
+const generateAccessAndRefreshToken = async (userId) => {
     try {
         const user = await User.findById(userId)
         const accessToken = user.generateAccessToken()
         const refreshToken = user.generateRefreshToken()
         user.refreshToken = refreshToken;
-        await user.save({ validateBeforeSave : false });
+        await user.save({ validateBeforeSave: false });
 
-        return {accessToken, refreshToken}
+        return { accessToken, refreshToken }
 
     } catch (error) {
         throw new ApiError(500, "Somthing went wrong while generating refresh and access token")
     }
 }
 
-const registerUser = asyncHandler(async (req, res)=>{
+const registerUser = asyncHandler(async (req, res) => {
 
-    const {username, email, fullname, password} = req.body
+    const { username, email, fullname, password } = req.body
 
-    if([username, email, fullname, password].some((field)=> field.trim() === ""))
-    {
+    if ([username, email, fullname, password].some((field) => field.trim() === "")) {
         throw new ApiError(400, "all fields are requierd")
     }
 
     const existedUser = await User.findOne({
-        $or: [{username}, {email}]
+        $or: [{ username }, { email }]
     })
 
-    if(existedUser){
+    if (existedUser) {
         throw new ApiError(401, "The user with username and email is already exist")
     }
 
@@ -50,75 +49,75 @@ const registerUser = asyncHandler(async (req, res)=>{
         password
     })
 
-    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshtoken"
     )
 
-    if(!createdUser){
+    if (!createdUser) {
         throw new ApiError(500, "Somthing went wrong while registering the user")
     }
 
     return res
-    .status(200)
-    .coockie("accesstoken", accessToken, options)
-    .coockie("refreshtoken", refreshToken, options)
-    .json(
-        new ApiResponse(200, {createdUser, accessToken, refreshToken}, "The user is registered successfully")
-    )
+        .status(200)
+        .coockie("accesstoken", accessToken, options)
+        .coockie("refreshtoken", refreshToken, options)
+        .json(
+            new ApiResponse(200, { createdUser, accessToken, refreshToken }, "The user is registered successfully")
+        )
 })
 
-const loginUser = asyncHandler(async (req, res)=>{
-    const {username, email, password} = req.body
+const loginUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body
 
-    if(!(username || email)){
+    if (!(username || email)) {
         throw new ApiError(400, "Username or email is required")
     }
 
     const orConditions = []
 
-    if(username) orConditions.push({username})
-    if(email) orConditions.push({email})
-    
+    if (username) orConditions.push({ username })
+    if (email) orConditions.push({ email })
+
     const user = await User.findOne({
         $or: orConditions
     })
 
-    if(!user) throw new ApiError(404, "User not found")
+    if (!user) throw new ApiError(404, "User not found")
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
-    if(!isPasswordValid) throw new ApiError(401, "Password is incorrect")
+    if (!isPasswordValid) throw new ApiError(401, "Password is incorrect")
 
-    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
-    
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
     const loggedInUser = await User.findById(user._id).select(
         "-password -refreshtoken"
     )
 
     return res
-    .status(200)
-    .coockie("accesstoken", accessToken, options)
-    .coockie("refreshtoken", refreshToken, options)
-    .json(
-        new ApiResponse(
-            200,
-            {
-                user: loggedInUser,
-                accessToken,
-                refreshToken
-            },
-            "User logged in successfully"
+        .status(200)
+        .coockie("accesstoken", accessToken, options)
+        .coockie("refreshtoken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+            )
         )
-    )
 })
 
-const logoutUser = asyncHandler(async (req, res)=>{
+const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {refreshToken: undefined}
+            $set: { refreshToken: undefined }
         },
         {
             new: true
@@ -126,16 +125,61 @@ const logoutUser = asyncHandler(async (req, res)=>{
     )
 
     res
-    .status(200)
-    .clearCookies("accessToken", options)
-    .clearCookies("refreshToken", options)
-    .json(
-        new ApiResponse(200, {}, "User logged out")
-    )
+        .status(200)
+        .clearCookies("accessToken", options)
+        .clearCookies("refreshToken", options)
+        .json(
+            new ApiResponse(200, {}, "User logged out")
+        )
+})
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.coockie.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+        if (!decodedToken) {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        const user = await User.findById(decodedToken?._id)
+
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            200,
+            {
+                accessToken,
+                refreshToken: newRefreshToken
+            },
+            "Access token is refreshed"
+        )
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
 })
 
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 } from "../controllers/user.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -14,4 +15,5 @@ router.route("/login").post(loginUser)
 
 //Secure routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
